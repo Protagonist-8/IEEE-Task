38b7d693d83e564c883cefdb4de63a7cd9c29554
Protagonist-8 <122916232+Protagonist-8@users.noreply.github.com>
2023-03-19 18:31:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ let toDoContainer = document.getElementById('TODOC');
 let inputField = document.getElementById('inputu');
 
 addToDoButton.addEventListener('click', function(){
-    const list = document.createElement('li');
-    list.innerText = inputField.value;
-    toDoContainer.appendChild(list);
-    inputField.value = "";
+    var divi = document.createElement('p');
+    if(inputField.value!=""){
+      divi.innerText = inputField.value;
+      toDoContainer.appendChild(divi);
+      inputField.value="";
+    var deleteButton = document.createElement('button');
+        deleteButton.id = 'delete-button';
+        deleteButton.innerHTML="Delete";
+        toDoContainer.appendChild(deleteButton);
+        deleteButton.addEventListener('click',function(){
+          toDoContainer.removeChild(divi);
+          deleteButton.remove('button');
+        })
+      }
+
+
 
 })
